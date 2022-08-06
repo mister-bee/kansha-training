d868ca8af645fe056bddda6cf3b6d0ed9adf85d7
mister-bee <thebeech@yahoo.com>
2022-08-06 17:10:43 -0500

    update next.config.js with /kansha-training

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: ""
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages"
+  basePath: "/kansha-training",
+  assetPrefix: "/kansha-training"
 }
 
 module.exports = nextConfig

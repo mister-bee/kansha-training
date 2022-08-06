382abb5939ed042ce53fd9edbe642e8d4d2d3e0e
mister-bee <thebeech@yahoo.com>
2022-08-06 17:29:55 -0500

    update title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          How to use <a href="https://kansha.app">Kansha*!*!</a>
+          How to use <a href="https://kansha.app">Kansha!!</a>
         </h1>
 
         <p className={styles.description}>
